482bf5af55fd3cc2bcb6149bf373e79a501815a4
Hukka <rhlehfndvkd7557@daum.net>
2022-10-01 16:41:24 +0900

    init: init project

---
+++ src/main/java/com/kw/kwdn/KwdnApplication.java
@@ -0,0 +1,13 @@
+package com.kw.kwdn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwdnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KwdnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kw/kwdn/KwdnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kw.kwdn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KwdnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
