7b0a273806f9cbe72f0ff250b13d5edbcdda7147
Hukka <rhlehfndvkd7557@daum.net>
2022-10-21 23:31:24 +0900

    feat : anonymous party api endpoint feat

--- src/main/java/com/kw/kwdn/domain/anonymousparty/controller/AnonymousPartyController.java
+++ src/main/java/com/kw/kwdn/domain/anonymousparty/controller/AnonymousPartyController.java
@@ -4,10 +4,10 @@ import com.kw.kwdn.domain.anonymousparty.dto.AnonymousPartyCreateDTO;
 import com.kw.kwdn.domain.anonymousparty.dto.AnonymousPartyDTO;
 import com.kw.kwdn.domain.anonymousparty.service.AnonymousPartyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/party")
@@ -16,7 +16,16 @@ public class AnonymousPartyController {
     private final AnonymousPartyService anonymousPartyService;
 
     @PostMapping("/create")
-    public AnonymousPartyDTO create(@RequestBody AnonymousPartyCreateDTO dto){
+    public AnonymousPartyDTO create(@RequestBody AnonymousPartyCreateDTO dto) {
         return anonymousPartyService.create(dto);
+    }
+
+    @PostMapping("")
+    public Page<AnonymousPartyDTO> findAll(
+            @RequestParam(name = "page", defaultValue = "10") Integer page,
+            @RequestParam(name = "size", defaultValue = "10") Integer size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        return anonymousPartyService.findAll(pageable);
     }
 }
--- src/main/java/com/kw/kwdn/domain/anonymousparty/service/AnonymousPartyService.java
+++ src/main/java/com/kw/kwdn/domain/anonymousparty/service/AnonymousPartyService.java
@@ -1,17 +1,27 @@
 package com.kw.kwdn.domain.anonymousparty.service;
 
+import com.kw.kwdn.domain.anonymousparty.AnonymousParty;
 import com.kw.kwdn.domain.anonymousparty.dto.AnonymousPartyCreateDTO;
 import com.kw.kwdn.domain.anonymousparty.dto.AnonymousPartyDTO;
 import com.kw.kwdn.domain.anonymousparty.repository.AnonymousPartyRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AnonymousPartyService {
     private final AnonymousPartyRepository anonymousPartyRepository;
 
+    @Transactional
     public AnonymousPartyDTO create(AnonymousPartyCreateDTO dto){
         return anonymousPartyRepository.save(dto.toEntity()).toDTO();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<AnonymousPartyDTO> findAll(Pageable pageable){
+        return anonymousPartyRepository.findAll(pageable).map(AnonymousParty::toDTO);
     }
 }
