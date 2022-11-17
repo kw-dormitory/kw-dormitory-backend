b6b3ee4440d4635a99f84a6efcc28ed79fbe27a5
tianea2160 <rhlehfndvkd7557@daum.net>
2022-11-17 22:23:32 +0900

    refactor : optimize import code

--- src/test/java/com/kw/kwdn/domain/member/controller/MemberControllerTest.java
+++ src/test/java/com/kw/kwdn/domain/member/controller/MemberControllerTest.java
@@ -7,18 +7,11 @@ import com.kw.kwdn.domain.security.dto.UserInfo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
