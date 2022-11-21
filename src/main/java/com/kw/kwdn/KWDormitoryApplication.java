813867cd8b6692922b8709e9a7a7e810cb4ebe73
tianea2160 <rhlehfndvkd7557@daum.net>
2022-11-21 11:09:47 +0900

    refactor : rename application class

--- src/main/java/com/kw/kwdn/KWDormitoryApplication.java
+++ src/main/java/com/kw/kwdn/KWDormitoryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KwdnApplication {
+public class KWDormitoryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KwdnApplication.class, args);
+        SpringApplication.run(KWDormitoryApplication.class, args);
     }
 
 }
