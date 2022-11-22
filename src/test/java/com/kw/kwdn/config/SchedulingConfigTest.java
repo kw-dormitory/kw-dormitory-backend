bd7bc90055a7001dc769968b17b2ff3339431e52
tianea2160 <rhlehfndvkd7557@daum.net>
2022-11-22 13:06:11 +0900

    fix : missing verify parameter error code fix

--- src/test/java/com/kw/kwdn/config/SchedulingConfigTest.java
+++ src/test/java/com/kw/kwdn/config/SchedulingConfigTest.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -52,7 +53,7 @@ public class SchedulingConfigTest {
         schedulingConfig.notice();
 
         // then
-        verify(firebaseService, times(0)).sendAlarm(commonTitle, any(), any());
+        verify(firebaseService, times(0)).sendAlarm(eq(commonTitle), any(), any());
         verify(noticeService, times(10)).existById(any());
     }
 
